87db4d90e86a0c196a6df675dd81759c750c3c29
FezDero <fezdero@gmail.com>
2024-12-16 21:39:48 +0500

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,31 @@
+def get_matrix (n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+result1 = get_matrix(3,3,10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4,4,13)
+
+    # 1 вариант решения, с указанием как заявлено в задаче - весь вывод в строку
+print (*result1)
+print (*result2)
+print (*result3)
+
+    # 2 вариант решения, с указанием как в задаче, но через цикл, и через * чтобы убрать квадратные
+    #скобки [] у списка matrix
+# for matrix in (result1, result2, result3):
+#     print(*matrix)
+
+    # 3 вариант решения, выводит каждую матрицу result1, result2, result3, с распаковкой
+
+# for matrix in result1:
+#     print (*matrix,  sep=' ')
+# for matrix in result2:
+#     print (*matrix, sep=' ')
+# for matrix in result3:
+#     print (*matrix, sep=' ')
+
+
